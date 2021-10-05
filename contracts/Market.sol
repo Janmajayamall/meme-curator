f445676dd6eccdd0b00e9b797a6d2ce55e0f5f3e
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-10-06 05:05:13 +0530

    fixed repititive vars

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -24,63 +24,32 @@ contract Market {
     address public immutable tokenC;
 
     address public immutable factory;
-
-    address public immutable oracle;
-    uint public immutable oracleFee;
-
     bytes32 public immutable identifier;
     uint public immutable expireAtBlock;
-
     address public creator;
+
 
     uint public outcome = 2;
     Stages public stage;
 
     // DON related
-    uint donBufferEndsAtBlock;
-    uint donBufferBlocks; 
-    uint donEscalationLimit;
-    uint256 reserveDoN0;
-    uint256 reserveDoN1;
+    uint256 public reserveDoN0;
+    uint256 public reserveDoN1;
     uint public lastOutcomeStaked = 2;
     uint public lastOutcomeAmountStaked;
     uint public donEscalationCount;
     uint public donEscalationLimit;
-    // 0 or 1 => staker => amount
-    mapping(bool => mapping(address => uint256)) stakes;
+    uint public donBufferEndsAtBlock;
+    uint public donBufferBlocks; 
+    // (0) true (1) -> staker's address => amount
+    mapping(address => uint256)[2] stakes;
 
     // final resolution related
-    uint  resolutionBufferBlocks;
-    uint resolutionEndsAtBlock;
-
-    // modifier isMarketStage(Stages _stage) {
-    //     if (stage == Stages.MarketResolve){
-    //         if (block.number >= resolutionEndsAtBlock){
-    //             // resolve the market to lasrt outcome
-    //             stage = Stages.MarketClosed;
-    //             require(_stage == Stages.MarketClosed);
-    //             _;
-    //             return;
-    //         }else {
-    //             require(_stage == Stages.MarketResolve);
-    //             _;
-    //             return;
-    //         }
-           
-    //     }
-    //     if (block.number >= expireAtBlock){
-    //         stage = Stages.MarketBuffer;
-    //     }
-    //     if (block.number >= donBufferEndsAtBlock){
-    //         stage = Stages.MarketClosed;
-    //     }
-    //     if (donEscalationCount > donEscalationLimit){
-    //         stage = Stages.MarketResolve;
-    //         resolutionEndsAtBlock = block.number + resolutionBufferBlocks;
-    //     }
-    //     require(stage == _stage);
-    //     _;
-    // }
+    address public immutable oracle;
+    uint public resolutionBufferBlocks;
+    uint public resolutionEndsAtBlock;
+    uint public immutable oracleFee;
+    uint public arbitrationFee;
 
     modifier isMarketCreated() {
         require (stage == Stages.MarketCreated);
@@ -137,9 +106,10 @@ contract Market {
         expireAtBlock = block.number.add(expireAfterBlocks);
     }
 
-    function getReserves() external view returns (uint _reserve0, uint _reserve1){
-        _reserve0 = reserve0;
-        _reserve1 = reserve1;
+    function getReservesTokenC() public view returns (uint _reserveC, uint _reserveDoN0, uint _reserveDoN1){
+        _reserveC = reserveC;
+        _reserveDoN0 = reserveDoN0;
+        _reserveDoN1 = reserveDoN1;
     }
 
     function tokensAddresses() external view returns (address _token0, address _token1){
@@ -180,128 +150,144 @@ contract Market {
         stage = Stages.MarketFunded;
     }
     
-    function buy(uint amount0, uint amount1) external isMarketFunded {
-        require(stage == Stages.MarketFunded);
+    function buy(uint amount0, uint amount1, address to) external isMarketFunded {
+        (uint _reserveC, uint _reserveDoN0, uint _reserveDoN1, address _tokenC) = getReservesTokenC();
+        (uint _reserve0, uint _reserve1, address _token0, address _token1) = getReservesTokenO();
 
         uint balance = IERC20(tokenC).balanceOf(address(this));
-        uint amount = balance.sub(reserveC);
+        uint amount = balance - (_reserveC + _reserveDoN0 + _reserveDoN1);
 
         // buying all tokens
-        OutcomeToken(token0).issue(address(this), amount);
-        OutcomeToken(token1).issue(address(this), amount);
+        OutcomeToken(_token0).issue(address(this), amount);
+        OutcomeToken(_token1).issue(address(this), amount);
 
         // transfer
-        if (amount0 > 0) OutcomeToken(token0).transfer(msg.sender, amount0);
-        if (amount1 > 0) OutcomeToken(token1).transfer(msg.sender, amount1);
+        if (amount0 > 0) OutcomeToken(_token0).transfer(to, amount0);
+        if (amount1 > 0) OutcomeToken(_token1).transfer(to, amount1);
 
-        uint rP = reserve0.mul(reserve1);
-        uint _reserve0 = reserve0.add(amount).sub(amount0);
-        uint _reserve1 = reserve1.add(amount).sub(amount1);
-        require(rP == _reserve0.mul(_reserve1));
+        uint rP = _reserve0.mul(_reserve1);
+        uint _reserve0New = (_reserve0 + amount) - amount0;
+        uint _reserve1New = (_reserve1 + amount) - amount1;
+        require(rP == _reserve0New.mul(_reserve1New));
 
-        reserve0 = _reserve0;
-        reserve1 = _reserve1;
-        reserveC = reserveC.add(amount);
+        reserve0 = _reserve0New;
+        reserve1 = _reserve1New;
+        reserveC += amount;
     }   
 
-    function sell(uint amount) external isMarketFunded {
-        require(stage == Stages.MarketFunded);
+    function sell(uint amount, address to) external isMarketFunded {
+        address _tokenC = tokenC;
+        (uint _reserve0, uint _reserve1, address _token0, address _token1) = getReservesTokenO();
 
-        IERC20(tokenC).transfer(msg.sender, amount);
+        IERC20(_tokenC).transfer(to, amount);
+
+        uint balance0 = OutcomeToken(_token0).balanceOf(address(this));
+        uint balance1 = OutcomeToken(_token1).balanceOf(address(this));
+        uint amount0 = balance0 - _reserve0;
+        uint amount1 = balance1 - _reserve1;
+
+        uint rP = _reserve0.mul(_reserve1);
+        uint _reserve0New = (_reserve0 + amount0) - amount;
+        uint _reserve1New = (_reserve1 + amount1) - amount;
+        require(rP == _reserve0New.mul(_reserve1New));
+
+        reserve0 = _reserve0New;
+        reserve1 = _reserve1New;
+        reserveC -= amount;
+    }
+
+    function redeemWinning(address to) external isMarketClosed {
+        uint _outcome = outcome;
 
         uint balance0 = OutcomeToken(token0).balanceOf(address(this));
         uint balance1 = OutcomeToken(token1).balanceOf(address(this));
-        uint amount0 = balance0.sub(reserve0);
-        uint amount1 = balance1.sub(reserve1);
-
-        uint rP = reserve0.mul(reserve1);
-        uint _reserve0 = reserve0.add(amount0).sub(amount);
-        uint _reserve1 = reserve1.add(amount1).sub(amount);
-        require(rP == _reserve0.mul(_reserve1));
-
-        reserve0 = _reserve0;
-        reserve1 = _reserve1;
-        reserveC = reserveC.sub(amount);
-    }
-
-    function redeemWinning(uint amount) external isMarketClosed {
-
-        IERC20(tokenC).transfer(msg.sender, amount);
-
-        uint balance0 = OutcomeToken(token0).balanceOf(address(this));
-        uint balance1 = OutcomeToken(token1).balanceOf(address(this));
-        uint amount0 = balance0.sub(reserve0);
-        uint amount1 = balance1.sub(reserve1);
+        uint amount0 = balance0 - reserve0;
+        uint amount1 = balance1 - reserve1;
 
         uint winnings;
-        if (outcome == 0){
-            winnings = winnings.add(amount0);
-        }else if(outcome == 1){
-            winnings = winnings.add(amount1);
-        }else if (outcome == 2){
-            winnings = winnings.add(amount0.div(2));
-            winnings = winnings.add(amount1.div(2));
-        }
-
-        require(amount == winnings);
-    }
-
-    function stakeOutcome(uint _for) external isMarketBuffer {
-        require (_for < 2);
-
-        uint balance = IERC20(tokenC).balanceOf(address(this));
-        uint amount = balance.sub(reserveDoN0.add(reserveDoN1));
-        if (_for == 0) {
-            stakes[false][msg.sender] = amount;
-            reserveDoN0 = reserveDoN0.add(amount);
-        }
-        if (_for == 1) {
-            stakes[true][msg.sender] = amount;
-            reserveDoN1 = reserveDoN1.add(amount);
-        }
-
-        if (lastOutcomeAmountStaked != 0) require(lastOutcomeAmountStaked.mul(2) == amount);
-        lastOutcomeAmountStaked = amount;
-        lastOutcomeStaked = _for;
-        donCount += 1;
-        bufferEndsByBlock = escalationBufferBlocks + block.number;
-    }
-
-    function redeemStake(uint _for) external isMarketClosed {
-        // if arb not called, then
-        //     amount is retieved
-
-        // else
-        //     amount is retieved
-        //     if (amount == lastOutcomeAmountStaked) then
-        //         send the money (arb caller lost)
-        //     else
-        //         don't send money
-
-        // haven't taken into account that market isn't resolved
-        require(outcome < 3 && _for < 2);
-        uint amount;
-        if (_for == 0) amount = stakes[false][msg.sender];
-        if (_for == 1) amount = stakes[true][msg.sender];
-
-        uint winnings;
-        if (outcome == 0 && _for == 0) {
-            if (amount == lastOutcomeAmountStaked) winnings = winnings.add(reserveDoN1);
-            winnings = winnings.add(amount);
-        } else if (outcome == 1 && _for == 0) {
-            if (amount == lastOutcomeAmountStaked) winnings = winnings.add(reserveDoN0);
-            winnings = winnings.add(amount);
-        } else {
-            winnings = amount;
+        if (_outcome == 0){
+            winnings += amount0;
+        }else if(_outcome == 1){
+            winnings += amount1;
+        }else if (_outcome == 2){
+            winnings += amount0.div(2);
+            winnings += amount1.div(2);
         }
 
         IERC20(tokenC).transfer(msg.sender, winnings);
     }
 
+    function stakeOutcome(uint _for, address to) external isMarketBuffer {
+        (uint _reserveC, uint _reserveDoN0, uint _reserveDoN1, address _tokenC) = getReservesTokenC(); 
+
+        uint balance = IERC20(_tokenC).balanceOf(address(this));
+        uint amount = balance - (_reserveDoN0 + _reserveDoN1 + _reserveC);
+
+        stakes[_for][to] += amount;
+        if (_for == 0) reserveDoN0 += amount;
+        if (_for == 1) reserveDoN1 += amount;
+
+        if (lastOutcomeAmountStaked != 0) require(lastOutcomeAmountStaked.mul(2) == amount);
+        lastOutcomeAmountStaked = amount;
+        lastOutcomeStaked = _for;
+        donEscalationCount += 1;
+        donBufferEndsAtBlock = donBufferBlocks + block.number;
+
+        require(lastOutcomeAmountStaked != 0);
+        require(_for < 2);
+    }
+
+    function redeemStake(uint _for) external isMarketClosed {
+        
+        uint amount = stakes[_for][msg.sender];
+        stakes[_for][msg.sender] = 0;
+        if (_for == 0) {
+            reserveDoN0 -= amount;
+        }
+        if (_for == 1) {
+            reserveDoN1 -= amount;
+        }
+
+        uint winnings;
+        if (outcome == _for) {
+            if (amount == lastOutcomeAmountStaked) {
+                winnings += reserveDoN1;
+            }
+            winnings += amount;
+        } else if (outcome == 1 && _for == 1) {
+            if (amount == lastOutcomeAmountStaked){
+                winnings += reserveDoN0;
+            } 
+            winnings += amount;
+        } else if (outcome == 2){
+            winnings = amount;
+        }
+
+        IERC20(tokenC).transfer(msg.sender, winnings);
+        require(_for < 2);
+    }
+
     function setOutcome(uint _outcome) external isMarketResolve {
-        require(msg.sender == oracle);
-        require(outcome < 3);
+        require(_outcome < 3);
+        
+        uint _oracleFee = oracleFee;
+        address _oracle = oracle;
+
+        if (_oracleFee != 0 && _outcome != 2){
+            uint fee;
+            if (_outcome == 0) {
+                fee = reserveDoN1.mul(_oracleFee).div(100);
+                reserveDoN1 -= fee;
+            }
+            if (_outcome == 1) {
+                fee = reserveDoN0.mul(_oracleFee).div(100);
+                reserveDoN0 -= fee;
+            }
+            IERC20(tokenC).transfer(_oracle, fee);
+        }
+        
         outcome = _outcome;
         stage = Stages.MarketClosed;
+        require(msg.sender == _oracle);
     }
 }
