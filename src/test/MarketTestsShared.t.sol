ebf4a95538e5ca9b474f0f222c56e42c8564173b
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-10-15 23:20:52 +0530

    organised into respective files

---
+++ src/test/MarketTestsShared.t.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+import './../libraries/Math.sol';
+import './../OracleMultiSig.sol';
+import './../MemeToken.sol';
+import './../MarketFactory.sol';
+import './../MarketRouter.sol';
+import './../libraries/Math.sol';
+import './../OutcomeToken.sol';
+
+contract MarketTestsShared is DSTest {
+
+    struct OracleConfig {
+        bool isActive;
+        uint feeNum;
+        uint feeDenom;
+        address tokenC;
+        uint expireAfterBlocks;
+        uint donEscalationLimit;
+        uint donBufferBlocks;
+        uint resolutionBufferBlocks;
+    }
+
+    struct StakingInfo {
+        uint lastOutcomeStaked;
+        uint lastAmountStaked;
+        uint[2] stakeAmounts;
+    }
+
+    address memeToken;
+    address oracle;
+    address marketFactory;
+    address marketRouter;
+    address marketAddress;
+
+    OracleConfig sharedOracleConfig;
+    StakingInfo simStakingInfo;
+    uint sharedFundingAmount = 1*10**18;
+
+    address hevm = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+
+    function commonSetup() internal {
+        marketFactory = address(new MarketFactory());
+        marketRouter = address(new MarketRouter(marketFactory));
+
+        memeToken = address(new MemeToken());
+        MemeToken(memeToken).mint(address(this), type(uint).max);
+
+        address[] memory oracleOwners = new address[](1);
+        oracleOwners[0] = address(this);
+        oracle = address(new OracleMultiSig(oracleOwners, 1, 10));
+        sharedOracleConfig = OracleConfig(true, 10, 100, memeToken, 10, 5, 10, 10);
+        OracleMultiSig(oracle).addTxSetupOracle(
+            sharedOracleConfig.isActive,
+            sharedOracleConfig.feeNum,
+            sharedOracleConfig.feeDenom,
+            sharedOracleConfig.tokenC,
+            sharedOracleConfig.expireAfterBlocks,
+            sharedOracleConfig.donEscalationLimit,
+            sharedOracleConfig.donBufferBlocks,
+            sharedOracleConfig.resolutionBufferBlocks
+        );
+    }
+
+    function createDefaultMarket() internal {
+        MemeToken(memeToken).approve(marketFactory, sharedFundingAmount);
+        bytes32 _identifier = 0x0401030400040101040403020201030003000000010202020104010201000103;
+        MarketFactory(marketFactory).createMarket(address(this), oracle, _identifier, sharedFundingAmount);
+        marketAddress = MarketRouter(marketRouter).getMarketAddress(address(this), oracle, _identifier);        
+    }
+
+    function getMarketContractInitBytecodeHash() internal returns (bytes32 initHash){
+        bytes memory initCode = type(Market).creationCode;
+        initHash = keccak256(initCode);
+    }
+
+    function redeemStake(uint _for, uint outcome, uint stakeAmount, uint expectedWinning) internal {
+        uint tokenCBalanceBefore = MemeToken(memeToken).balanceOf(address(this));
+        Market(marketAddress).redeemStake(_for);
+        uint tokenCBalanceAfter = MemeToken(memeToken).balanceOf(address(this));
+
+        if (_for == outcome){
+            assertEq(tokenCBalanceAfter, tokenCBalanceBefore + stakeAmount + expectedWinning);
+        }else if (outcome == 2){
+            assertEq(tokenCBalanceAfter, tokenCBalanceBefore + stakeAmount);
+        }else {
+            assertEq(tokenCBalanceAfter, tokenCBalanceBefore);
+        }
+    }
+
+    function redeemWinning(uint _for, uint tokenAmount, uint _outcome) internal {
+        (, address token0, address token1) = Market(marketAddress).getAddressOfTokens();
+        if (_for == 0){
+            OutcomeToken(token0).transfer(marketAddress, tokenAmount);
+        }else if (_for == 1){
+            OutcomeToken(token1).transfer(marketAddress, tokenAmount);
+        }
+        uint tokenCBalanceBefore = MemeToken(memeToken).balanceOf(address(this));
+        Market(marketAddress).redeemWinning(_for, address(this));
+        uint tokenCBalanceAfter = MemeToken(memeToken).balanceOf(address(this));
+
+        uint expectedWin;
+        if (_outcome == 2){
+            expectedWin = tokenAmount/2;
+        }else if (_outcome == _for){
+            expectedWin = tokenAmount;
+        }
+
+        assertEq(tokenCBalanceAfter, tokenCBalanceBefore+expectedWin);
+    }
+
+    function simTradesInFavorOfOutcome0() internal {
+        uint a0 = 10*10**18;
+        uint a1 = 0;
+        uint a = Math.getAmountCToBuyTokens(a0, a1, sharedFundingAmount, sharedFundingAmount);
+        MemeToken(memeToken).transfer(marketAddress, a);
+        Market(marketAddress).buy(a0, a1, address(this));
+        a0 = 0;
+        a1 = 4*10**18;
+        a = Math.getAmountCToBuyTokens(a0, a1, sharedFundingAmount+a-10*10**18, sharedFundingAmount+a);
+        MemeToken(memeToken).transfer(marketAddress, a);
+        Market(marketAddress).buy(a0, a1, address(this));
+    }
+
+    function simTradesInFavorOfOutcome1() internal {
+        uint a1 = 10*10**18;
+        uint a0 = 0;
+        uint a = Math.getAmountCToBuyTokens(a0, a1, sharedFundingAmount, sharedFundingAmount);
+        MemeToken(memeToken).transfer(marketAddress, a);
+        Market(marketAddress).buy(a0, a1, address(this));
+        a1 = 0;
+        a0 = 4*10**18;
+        a = Math.getAmountCToBuyTokens(a0, a1, sharedFundingAmount+a, sharedFundingAmount+a-10*10**18);
+        MemeToken(memeToken).transfer(marketAddress, a);
+        Market(marketAddress).buy(a0, a1, address(this));
+    }
+
+    function simStakingRoundsTillEscalationLimit(uint escalationLimit) internal {
+        simStakingInfo.lastAmountStaked = 0;
+        simStakingInfo.lastOutcomeStaked = 2;
+        simStakingInfo.stakeAmounts[0] = 0;
+        simStakingInfo.stakeAmounts[1] = 0;
+        for (uint index = 0; index < escalationLimit; index++) {
+            uint _amount = (2*10**18)*(2**index);
+            uint _outcome = index % 2;
+            MemeToken(memeToken).transfer(marketAddress, _amount); 
+            Market(marketAddress).stakeOutcome(_outcome, address(this));
+            simStakingInfo.lastOutcomeStaked = _outcome;
+            simStakingInfo.lastAmountStaked = _amount;
+            simStakingInfo.stakeAmounts[_outcome] += _amount;
+        }
+    }
+
+    function simStakingRoundsBeforeEscalationLimit(uint escalationLimit) internal {
+        simStakingInfo.lastAmountStaked = 0;
+        simStakingInfo.lastOutcomeStaked = 2;
+        simStakingInfo.stakeAmounts[0] = 0;
+        simStakingInfo.stakeAmounts[1] = 0;
+        for (uint index = 0; index < escalationLimit-1; index++) {
+            uint _amount = (2*10**18)*(2**index);
+            uint _outcome = index % 2;
+            MemeToken(memeToken).transfer(marketAddress, _amount); 
+            Market(marketAddress).stakeOutcome(_outcome, address(this));
+            simStakingInfo.lastOutcomeStaked = _outcome;
+            simStakingInfo.lastAmountStaked = _amount;
+            simStakingInfo.stakeAmounts[_outcome] += _amount;
+        }
+    }
+
+    function expireMarket() virtual internal {
+        (bool success, bytes memory data) = hevm.call(abi.encodeWithSignature("roll(uint256)", block.number+10));
+    }
+
+    function expireBufferPeriod() virtual internal {
+        // expire market
+        (bool success, bytes memory data) = hevm.call(abi.encodeWithSignature("roll(uint256)", block.number+10));
+    }
+
+    function setUp() virtual public {
+        commonSetup();
+    }
+}
