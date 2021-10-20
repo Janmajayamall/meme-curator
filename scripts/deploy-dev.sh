380b3fc811b98338c228960d26417c9caf192f91
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-10-20 19:09:53 +0530

    removed unecessary deploy

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -23,24 +23,3 @@ log "MarketRouter deployed at:" $MarketRouter
 log "OracleMultiSig deployed at:" $OracleMultiSig
 log "MemeToken deployed at:" $MemeToken
 log "ContractHelper deployed at:" $ContractHelper
-
-marketInitCodehash=$(seth call $ContractHelper "getMarketContractInitBytecodeHash()")
-log "Market.sol init code hash" $marketInitCodehash
-
-
-seth send $OracleMultiSig "addTxSetupOracle(bool,uint256,uint256,address,uint256,uint256,uint256,uint256)" true 10 10 $MemeToken 10 10 10 10 --gas 550000
-
-# mint max meme tokens to user
-seth send $MemeToken "mint(address,uint256)" $ETH_FROM $(seth --max-uint 256)
-# give mmax allowance to market router
-seth send $MemeToken "approve(address,uint256)" $MarketRouter $(seth --max-uint 256)
-
-Identifier=$(seth --to-bytes32 0x0401030400040101040403020201030003000000010202020104010201000103)
-Funding=$(seth --to-wei 1 eth)
-
-estimate=$(seth estimate $MarketRouter "createMarket(address,address,bytes32,uint256)" $ETH_FROM $OracleMultiSig $Identifier $Funding)
-seth send $MarketRouter "createMarket(address,address,bytes32,uint256)" $ETH_FROM $OracleMultiSig $Identifier $Funding --gas $estimate
-log "MarketRouter:createMarket " $estimate
-
-estimate=$(seth estimate $MarketRouter "buyExactTokensForMaxCTokens(uint256,uint256,uint256,address,address,bytes32)" $Funding $Funding $Funding $ETH_FROM $OracleMultiSig $Identifier)
-log "MarketRouter:buyExactTokensForMaxCTokens " $estimate
