4dcdb79f321f2d61565db74120eb98ee62cb70cf
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-11-03 23:56:48 +0800

    gas changes

--- scripts/gas-test.sh
+++ scripts/gas-test.sh
@@ -8,26 +8,30 @@ set -eo pipefail
 # OracleMultiSig=$(jq ".OracleMultiSig" out/addresses.json)
 # MemeToken=$(jq ".MemeToken" out/addresses.json)
 # ContractHelper=$(jq ".ContractHelper" out/addresses.json)
-MarketFactory=0xD4D664D419A6A845C98Cc366ae1c4b24592BD5CE
-MarketRouter=0x49018BD5F5CB62023c2945f30C6aa25c9F0C7249
-OracleMultiSig=0x62862436A93B538b7820E0F24E0b6F9e3f9C027d
-MemeToken=0xFB626449A112C95FECF7829deeA9445C9ECC7e56
-ContractHelper=0xD1F7c8dde5F1d9d8691BF5ee03559D1767324717
+MarketFactory=0x36beE43e0548cfAa63e8094895b2Ede098e36B46
+MarketRouter=0x8EfE5a96eaE05D72492d9A0296794adB0EF76d78
+OracleMultiSig=0x2d102ED735c39F2060D9057056eC68e1430744de
+MemeToken=0xB255efe9889D45277D3421e289E2E8A37Fe5b180
+ContractHelper=0x8B4571514AB4d420Af057929654D89A4AFd70E7c
 DEPLOYER=0xed53fa304E7fcbab4E8aCB184F5FC6F69Ed54fF6
 
-seth send 0xD4D664D419A6A845C98Cc366ae1c4b24592BD5CE "createMarket(address,address,bytes32,uint256)" 0xed53fa304E7fcbab4E8aCB184F5FC6F69Ed54fF6 0x62862436A93B538b7820E0F24E0b6F9e3f9C027d $(seth --to-bytes32 $(seth --to-hex 100)) $(seth --to-wei 1 eth) --gas 4364615
 
 # # mint max meme tokens to user
-# estimate=$(seth estimate $MemeToken "mint(address,uint256)" $ETH_FROM $(seth --max-uint 256) )
-# seth send $MemeToken "mint(address,uint256)" $ETH_FROM $(seth --max-uint 256) --gas $estimate
+estimate=$(seth estimate $MemeToken "mint(address,uint256)" $DEPLOYER $(seth --max-uint 256) )
+seth send $MemeToken "mint(address,uint256)" $DEPLOYER $(seth --max-uint 256) --gas $estimate
 # # give mmax allowance to market router & market factory
-# estimate=$(seth estimate $MemeToken "approve(address,uint256)" $MarketRouter $(seth --max-uint 256))
-# seth send $MemeToken "approve(address,uint256)" $MarketRouter $(seth --max-uint 256) --gas $estimate
+estimate=$(seth estimate $MemeToken "approve(address,uint256)" $MarketRouter $(seth --max-uint 256))
+seth send $MemeToken "approve(address,uint256)" $MarketRouter $(seth --max-uint 256) --gas $estimate
 # seth send $MemeToken "approve(address,uint256)" $MarketFactory $(seth --max-uint 256) --gas $estimate
 
 # setup orcle configs
-estimate=$(seth estimate $OracleMultiSig "addTxSetupOracle(address,bool,uint8,uint8,uint16,uint32,uint32,uint32)" $MemeToken true 1 10 5 100 100 100)
-seth send $OracleMultiSig "addTxSetupOracle(address,bool,uint8,uint8,uint16,uint32,uint32,uint32)" $MemeToken true 1 10 5 100 100 100 --gas $estimate
+estimate=$(seth estimate $OracleMultiSig "addTxSetupOracle(address,bool,uint8,uint8,uint16,uint32,uint32,uint32)" $MemeToken true 1 10 5 500 100 100)
+seth send $OracleMultiSig "addTxSetupOracle(address,bool,uint8,uint8,uint16,uint32,uint32,uint32)" $MemeToken true 1 10 5 500 100 100 --gas $estimate
+
+# create new market
+estimate=$(seth estimate $MarketRouter "createAndPlaceBetOnMarket(address,address,bytes32,uint256,uint256,uint256)" $DEPLOYER $OracleMultiSig $(seth --to-bytes32 $(seth --to-hex 11413)) $(seth --to-wei 1 eth) $(seth --to-wei 1 eth) 1)
+seth send $MarketRouter "createAndPlaceBetOnMarket(address,address,bytes32,uint256,uint256,uint256)" $DEPLOYER $OracleMultiSig $(seth --to-bytes32 $(seth --to-hex 11413)) $(seth --to-wei 1 eth) $(seth --to-wei 1 eth) 1 --gas $estimate
+
 
 
 # marketInitCodehash=$(seth call $ContractHelper "getMarketContractInitBytecodeHash()")
@@ -60,12 +64,13 @@ seth send $OracleMultiSig "addTxSetupOracle(address,bool,uint8,uint8,uint16,uint
 
 # seth send 0x4789008BBa817d16904Dc4c9273e319c1625c331 "buyExactTokensForMaxCTokens(uint256,uint256,uint256,address)" $(seth --to-wei 1 eth) $(seth --to-wei 1 eth) $(seth --to-wei 1.1 eth) 0x95affa24b28122797b83a411fdc0fcff75a66f19
 
-seth estimate 0x49018BD5F5CB62023c2945f30C6aa25c9F0C7249 "createAndPlaceBetOnMarket(address,address,bytes32,uint256,uint256)" 0xed53fa304E7fcbab4E8aCB184F5FC6F69Ed54fF6 0x62862436A93B538b7820E0F24E0b6F9e3f9C027d $(seth --to-bytes32 $(seth --to-hex 11413)) $(seth --to-wei 1 eth) $(seth --to-wei 1 eth)
 
-1. Start working on backend for posting memes & retreiving memes
-APIs that are needed 
-a. Post meme (should not allow posting meme, if the request does not contains valid values relating to creation of prediction tx)
-b. Get feed of memes -
-    ~ Will return a list of memes in a category (probably Meme data & prediction market id)
-    ~ Probably on the front end - retrieve the list of memes from back end & superimpose them with data of perdiction markets
-c. Handle normal updates to meme data (for ex. likes & dislikes)
+# seth send 0xFB626449A112C95FECF7829deeA9445C9ECC7e56 "approve(address,uint256)" 0xD2Ac5854F87e2aF6c0c2dEc8f31B4588A5093131 $(seth --max-uint 256) --gas 124841
+
+# 1. Start working on backend for posting memes & retreiving memes
+# APIs that are needed 
+# a. Post meme (should not allow posting meme, if the request does not contains valid values relating to creation of prediction tx)
+# b. Get feed of memes -
+#     ~ Will return a list of memes in a category (probably Meme data & prediction market id)
+#     ~ Probably on the front end - retrieve the list of memes from back end & superimpose them with data of perdiction markets
+# c. Handle normal updates to meme data (for ex. likes & dislikes)
