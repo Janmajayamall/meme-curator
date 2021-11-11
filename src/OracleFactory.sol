5de707963895317fe729d1bb000f0c74fb5f4feb
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-11-11 23:49:14 +0800

    added update oracle

--- src/OracleFactory.sol
+++ src/OracleFactory.sol
@@ -34,6 +34,33 @@ contract OracleFactory {
         emit OracleRegistered(oracle);
     }
 
+    function updateOracle(
+        address _tokenC, 
+        bool _isActive, 
+        uint8 _feeNumerator, 
+        uint8 _feeDenominator,
+        uint16 _donEscalationLimit, 
+        uint32 _expireBufferBlocks, 
+        uint32 _donBufferBlocks, 
+        uint32 _resolutionBufferBlocks,
+        address oracleAddress
+    ) external {
+        (bool success,) = oracleAddress.call(abi.encodeWithSignature("updateParams(address,bool,uint8,uint8,uint16,uint32,uint32,uint32)",
+            _tokenC, 
+            _isActive, 
+            _feeNumerator, 
+            _feeDenominator,
+            _donEscalationLimit, 
+            _expireBufferBlocks, 
+            _donBufferBlocks, 
+            _resolutionBufferBlocks,
+            oracleAddress
+            )
+        );
+        require(success);
+        emit OracleRegistered(oracleAddress);
+    }
+
     function registerOracle(address oracle) external {
         address delegate = IModerationCommitte(oracle).getDelegate();
         require(delegate != address(0));
